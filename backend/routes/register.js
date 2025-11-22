8ab91644a3c995392ddbc54aa5372725341c9e41
Abu Russel <144946850+aburussel87@users.noreply.github.com>
2025-11-22 22:59:17 +0600

    Add user_id generation and insert into users table

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -8,6 +8,12 @@ const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
+function generateUserId(phone) {
+  const last4 = phone.slice(-4);               // last 4 digits of phone number
+  const rand = Math.floor(1000 + Math.random() * 9000); // random 4 digits
+  return Number(`${last4}${rand}`);            // 8-digit number
+}
+
 router.post("/", upload.single("profile_image"), async (req, res) => {
   try {
     const {
@@ -26,10 +32,10 @@ router.post("/", upload.single("profile_image"), async (req, res) => {
     } = req.body;
 
     const hashedPassword = await bcrypt.hash(password, 10);
-
+    const user_id = generateUserId(phone);
     const userResult = await pool.query(
-      `INSERT INTO users (full_name, username, email, phone, password_hash, gender, profile_image)
-       VALUES ($1, $2, $3, $4, $5, $6, $7)
+      `INSERT INTO users (full_name, username, email, phone, password_hash, gender, profile_image,user_id)
+       VALUES ($1, $2, $3, $4, $5, $6, $7,$8)
        RETURNING user_id`,
       [
         full_name,
@@ -39,15 +45,14 @@ router.post("/", upload.single("profile_image"), async (req, res) => {
         hashedPassword,
         gender,
         req.file ? req.file.buffer : null,
+        user_id
       ]
     );
-
-    const userId = userResult.rows[0].user_id;
 
     await pool.query(
       `INSERT INTO addresses (user_id, address_line1, address_line2, division, city, state, postal_code, country, is_default)
        VALUES ($1, $2, $3, $4, $5, $6, $7, 'Bangladesh', true)`,
-      [userId, address_line1, address_line2, division, district, upazila, postal_code]
+      [user_id, address_line1, address_line2, division, district, upazila, postal_code]
     );
 
     return res.status(201).json({ message: "User registered successfully" });
